86f4ab1cb709956d4ba7056fd706439d02c0570d
gavin crowder <gcrowder@icstars.org>
2016-08-23 18:39:18 +0000

    created routes for view pages, deleted unused files

--- backend/routes.js
+++ backend/routes.js
@@ -6,14 +6,32 @@ var database = require('./c36_modules/database');
 module.exports = function() {
 
   router.get('/', function(req,res){
-    return res.render('helloworld.html');
+    return res.render('homepage.html');
   });
 
-  router.get('/helloworld', function(req,res){
-    return res.render('helloworld.html');
+  router.get('/register', function(req,res){
+    return res.render('register.html');
   });
-
-  /* Your code here */
+  
+  router.get('/login', function(req,res){
+    return res.render('login.html');
+  });
+  
+  router.get('/about', function(req,res){
+    return res.render('aboutpage.html');
+  });
+  
+  router.get('/game', function(req,res){
+    return res.render('gamingarena.html');
+  });
+  
+  router.get('/settings', function(req,res){
+    return res.render('settings.html');
+  });
+  
+  router.get('/profile', function(req,res){
+    return res.render('Playerprofile.html');
+  });
 
   return router
 }();
--- frontend/js/helloworld.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-$(document).ready(function(){
-	$('#hiddenMessageBtn').click(function(val){
-		var name = $('#name').val();
-		$('#hiddenMessage').show();
-		$('#insertName').html(name);
-	});
-});
